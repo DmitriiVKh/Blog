db6f36c2389a8ff569c4b53cb3a8a2f266111017
DmitriiVKh <hdv.kub@gmail.com>
2024-02-14 00:30:24 +0300

    ready to start

---
+++ Blog/src/App.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+text-align: center;
+`
+export const App = () => {
+
+	return (
+
+		<Div>
+			<i className="fa fa-camera-retro"> </i>
+			<div>123</div>
+		</Div>
+
+
+	);
+}
+
+export default App;
+
