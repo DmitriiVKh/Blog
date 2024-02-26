3cace92651d473d44664b5196b6f2cbcbb9e0f60
DmitriiVKh <hdv.kub@gmail.com>
2024-02-27 00:15:12 +0300

    Реализовать страницу Регистрация

--- Blog/src/pages/registration/registration.jsx
+++ Blog/src/pages/registration/registration.jsx
@@ -1,19 +1,18 @@
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { server } from '../../bff';
 import { useState } from 'react';
 import { AuthFormError, Input, Button, H2 } from '../../components';
-export { useResetForm } from '../../hooks';
+import { useResetForm } from '../../hooks';
 import styled from 'styled-components';
 import { setUser } from '../../actions';
 import { selectUserRole } from '../../selectors';
 import { ROLE } from '../../constants';
-import { useResetForm } from '../../hooks';
 
-const authFormSchema = yup.object().shape({
+const regFormSchema = yup.object().shape({
 	login: yup
 		.string()
 		.required('Заполните логин')
@@ -29,14 +28,11 @@ const authFormSchema = yup.object().shape({
 		)
 		.min(6, 'Неверно заполнен пароль. Минимум 6 символа')
 		.max(30, 'Неверно заполнен пароль. Максимум 30 символа'),
+	passcheck: yup
+		.string()
+		.required('Заполните повтор пароля')
+		.oneOf([yup.ref('password'), null], 'Повтор пароля не совпадает'),
 });
-
-const StyledLink = styled(Link)`
-	text-align: center;
-	text-decoration: underline;
-	margin: 20px 0;
-	font-size: 18px;
-`;
 
 // const ErrorMessage = styled.div`
 // 	font-size: 18px;
@@ -45,7 +41,7 @@ const StyledLink = styled(Link)`
 // 	background-color: #fcadad;
 // `;
 
-const AuthorizationContainer = ({ className }) => {
+const RegistrationContainer = ({ className }) => {
 	const {
 		register,
 		reset,
@@ -55,8 +51,9 @@ const AuthorizationContainer = ({ className }) => {
 		defaultValues: {
 			login: '',
 			password: '',
+			passcheck: '',
 		},
-		resolver: yupResolver(authFormSchema),
+		resolver: yupResolver(regFormSchema),
 	});
 
 	const [serverError, setServerError] = useState(null);
@@ -84,21 +81,25 @@ const AuthorizationContainer = ({ className }) => {
 
 	const onSubmit = ({ login, password }) => {
 		server
-			.authorize(login, password)
+			.register(login, password)
 			.then((res) => {
-				if (res.error) {
+				if (res && res.error) {
 					setServerError(res.error);
 					return;
 				}
-
-				dispatch(setUser(res.res));
+				if (res && res.res) {
+					dispatch(setUser(res.res));
+				} else {
+					setServerError('Не удалось получить данные при регистрации');
+				}
 			})
 			.catch((error) => {
 				setServerError(`Ошибка запроса: ${error}`);
 			});
 	};
 
-	const formError = errors?.login?.message || errors?.password?.message;
+	const formError =
+		errors?.login?.message || errors?.password?.message || errors?.passcheck?.message;
 
 	const errorMessage = formError || serverError;
 
@@ -108,7 +109,7 @@ const AuthorizationContainer = ({ className }) => {
 
 	return (
 		<div className={className}>
-			<H2>Авторизация</H2>
+			<H2>Регистрацмя</H2>
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<Input
 					type="text"
@@ -122,17 +123,23 @@ const AuthorizationContainer = ({ className }) => {
 						onChange: () => setServerError(null),
 					})}
 				/>
+				<Input
+					type="password"
+					placeholder="Проверка пароля"
+					{...register('passcheck', {
+						onChange: () => setServerError(null),
+					})}
+				/>
 				<Button type="submit" disabled={!!formError}>
-					Авторизоваться
+					Зарегистрироваться
 				</Button>
 				{errorMessage && <AuthFormError>{errorMessage}</AuthFormError>}
-				<StyledLink to="/register">Регистрация</StyledLink>
 			</form>
 		</div>
 	);
 };
 
-export const Authorization = styled(AuthorizationContainer)`
+export const Registration = styled(RegistrationContainer)`
 	display: flex;
 	align-items: center;
 	flex-direction: column;
